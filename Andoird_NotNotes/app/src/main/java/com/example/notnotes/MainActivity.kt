4c63c5779e6e4071718e0a3da6522de310471e9d
Trần Hoài Bảo <52000634@student.tdtu.edu.vn>
2023-12-07 13:46:47 +0700

    Navigate to Profile Activity and Back

--- Andoird_NotNotes/app/src/main/java/com/example/notnotes/MainActivity.kt
+++ Andoird_NotNotes/app/src/main/java/com/example/notnotes/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.notnotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.example.notnotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,5 +21,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreateOptionsMenu(menu)
         menuInflater.inflate(R.menu.main_option_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_item_profile -> openProfileActivity()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun openProfileActivity() {
+        val profileIntent = Intent(this, ProfileActivity::class.java)
+        startActivity(profileIntent)
     }
 }
--- Andoird_NotNotes/app/src/main/java/com/example/notnotes/ProfileActivity.kt
+++ Andoird_NotNotes/app/src/main/java/com/example/notnotes/ProfileActivity.kt
@@ -2,10 +2,40 @@ package com.example.notnotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.example.notnotes.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.profile_option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            R.id.menu_item_back -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
