6149a62654328de0254f95462b28262279a5866a
Trần Hoài Bảo <52000634@student.tdtu.edu.vn>
2023-12-07 13:19:29 +0700

    Create main option menu

--- Andoird_NotNotes/app/src/main/java/com/example/notnotes/MainActivity.kt
+++ Andoird_NotNotes/app/src/main/java/com/example/notnotes/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.notnotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.example.notnotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_option_menu, menu)
+        return true
     }
 }
