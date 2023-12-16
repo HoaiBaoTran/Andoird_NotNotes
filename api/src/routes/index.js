3f257cabb42fd8132d137cbdf39abcf758e6b140
Trần Hoài Bảo <52000634@student.tdtu.edu.vn>
2023-12-17 00:50:34 +0700

    Change signup activity mysql to firebase, bug not check username, fix later

--- api/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const userRouter = require('./user.js')
-const taskRouter = require('./task.js')
-
-function route(app) {
-    app.use('/api/users', userRouter)
-    app.use('/api/tasks',taskRouter)
-}
-
-module.exports = route
