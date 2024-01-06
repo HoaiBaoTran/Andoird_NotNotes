34fbc3b2535db93aa15e720397d35d7e52e7e77a
Trần Hoài Bảo <52000634@student.tdtu.edu.vn>
2024-01-06 09:57:28 +0700

    Add nav bar for profile activity

--- Andoird_NotNotes/app/src/main/java/com/example/notnotes/userservice/ProfileActivity.kt
+++ Andoird_NotNotes/app/src/main/java/com/example/notnotes/userservice/ProfileActivity.kt
@@ -1,25 +1,38 @@
 package com.example.notnotes.userservice
 
 import android.content.Intent
+import android.graphics.Typeface
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import com.example.notnotes.R
+import com.example.notnotes.appservice.SettingActivity
 import com.example.notnotes.database.FirebaseService
 import com.example.notnotes.databinding.ActivityProfileBinding
 import com.example.notnotes.listener.FirebaseReadUserListener
 import com.example.notnotes.listener.FirebaseUpdateUserListener
 import com.example.notnotes.model.User
+import com.example.notnotes.noteservice.TrashActivity
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseUser
 import com.squareup.picasso.Picasso
 
 class ProfileActivity :
     AppCompatActivity(),
     FirebaseReadUserListener,
-    FirebaseUpdateUserListener {
+    FirebaseUpdateUserListener,
+    NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityProfileBinding
     private lateinit var database: FirebaseService
@@ -33,7 +46,19 @@ class ProfileActivity :
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolBar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val toggle: ActionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolBar,
+            R.string.nav_drawer_open,
+            R.string.nav_drawer_close)
+        toggle.drawerArrowDrawable.color = resources.getColor(R.color.white)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         NO_INFORMATION = getString(R.string.no_information)
 
@@ -164,9 +189,39 @@ class ProfileActivity :
 
     override fun onReadUserSuccess(user: User) {
         this.user = user
+        initNavHeaderView()
         loadUserInfo()
         loadUserImage()
         binding.progressBar.visibility = View.GONE
+    }
+
+    private fun initNavHeaderView() {
+        val userName = user.fullName
+//        val formattedString = getString(R.string.formatted_string, userName)
+        val spannableString = SpannableString(userName)
+
+//        val start = formattedString.indexOf(userName)
+        val start = 0
+        val end = start + userName.length
+        val styleSpan = StyleSpan(Typeface.BOLD)
+        spannableString.setSpan(styleSpan, start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        val colorSpan = ForegroundColorSpan(resources.getColor(R.color.lightgray))
+        spannableString.setSpan(colorSpan, start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        val headerView = binding.navigationView.getHeaderView(0)
+        val headerText: TextView = headerView.findViewById(R.id.tvNavHeader)
+        val imgProfile: ImageView = headerView.findViewById(R.id.imgProfile)
+        headerText.text = spannableString
+
+        val uri: Uri? = database.auth.currentUser?.photoUrl
+
+        if (uri != null) {
+            Picasso.with(this).load(uri)
+                .fit()
+                .centerCrop()
+                .into(imgProfile)
+        }
     }
 
     override fun onReadUserFailure() {
@@ -186,5 +241,67 @@ class ProfileActivity :
 
     override fun onUpdateUserFailure() {
 
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_notes -> {
+
+            }
+            R.id.nav_storage -> {
+
+            }
+            R.id.nav_label -> {
+
+            }
+            R.id.nav_recycle_bin -> {
+                openTrashActivity()
+            }
+            R.id.nav_profile -> {
+                openProfileActivity()
+            }
+            R.id.nav_change_pass -> {
+                openChangePasswordActivity()
+            }
+            R.id.nav_logout -> {
+                logoutAccount()
+            }
+            R.id.nav_setting -> {
+                openSettingActivity()
+            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun openTrashActivity() {
+        val trashIntent = Intent(this, TrashActivity::class.java)
+        startActivity(trashIntent)
+    }
+
+    private fun openSettingActivity() {
+        val settingIntent = Intent(this, SettingActivity::class.java)
+        startActivity(settingIntent)
+    }
+
+    private fun logoutAccount() {
+        deleteSharedPreferences("MyPreferences")
+        openLoginActivity()
+    }
+
+    private fun openChangePasswordActivity() {
+        val changePasswordIntent = Intent(this, ChangePasswordActivity::class.java)
+        startActivity(changePasswordIntent)
+    }
+
+    private fun openLoginActivity() {
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
+        finish()
+    }
+
+    private fun openProfileActivity() {
+        val profileIntent = Intent(this, ProfileActivity::class.java)
+        startActivity(profileIntent)
     }
 }
